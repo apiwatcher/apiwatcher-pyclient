341ae99794c0d80f819c67b9a461b289985c5591
Karel Jakubec <karel@apiwatcher.com>
2017-05-17 23:57:33 +0200

    Create 0.1.0 version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='Apiwatcher-Pyclient',
-    version='0.0.3',
+    version='0.1.0',
 
     description='A simple client for Apiwatcher platform',
     long_description=long_description,
